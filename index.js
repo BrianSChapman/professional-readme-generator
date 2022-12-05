576f1767ed534643f2ba45efed5c7d1cbb94cb64
Brian Chapman <chapman.brian86@gmail.com>
2022-12-05 17:26:53 -0500

    Writing the init function to get this thing up and running

--- index.js
+++ index.js
@@ -1,96 +1,88 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const generateMarkdown = require("./utils/generateMarkdown");
-
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
   inquirer.prompt([
-  {
-    type: "input",
-    message:"Hello! Please input your first and last name.",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is your GitHub username?",
-    name: "userName",
-  },
-  {
-    type: "input",
-    message: "What's your email address?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What is your project title?",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "Provide a short description of your project",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "Please provide installation instructions",
-    name: "installation",
-    default: "none",
-  },
-  {
-    type: "input",
-    message: "Provide instructions and examples for use",
-    name: "usage",
-  },
-  {
-    type: "input",
-    message: "Provide guidelines for how others can contribute to your project",
-    name: "contribute",
-  },
-  {
-    type: "input",
-    message: "Write tests for your application and provide details on how to run them here.",
-    name: "testing",
-    default:"none",
-  },
-  {
-    type: "confirm",
-    message: "Does your project have a license?",
-    name: "licenseConfirm",
-  },  
-  {
-    type: "list",
-    message: "Select your license below",
-    choices: [
-        "Apache License 2.0",
-        "GNU General Public License v3.0",
-        "MIT License",
-        "BSD 2-Clause 'Simplified' License",
-        "BSD 3-Clause 'New' or 'Revised' License",
-        "Boost Software License 1.0",
-        "Creative Commons Zero v1.0 Universal",
-        "Eclipse Public License 2.0",
-        "GNU Affero General Public License v3.0",
-        "GNU General Public License v2.0",
-        "Mozilla Public License 2.0",
-        "The Unlicense",],   
-    name: "license",
-    
-     },
+    {
+      type: 'input',
+      message: 'Hello! Please input your first and last name',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'What is your GitHub username?',
+      name: 'userName',
+    },
+    {
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Provide a short description of your project',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Please provide installation instructions',
+      name: 'installation',
+      default: 'none',
+    },
+    {
+      type: 'input',
+      message: 'Provide instructions and examples for use',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message:
+        'Provide guidelines for how others can contribute to your project',
+      name: 'contribute',
+    },
+    {
+      type: 'input',
+      message:
+        'Write tests for your application and provide details on how to run them here.',
+      name: 'testing',
+      default: 'none',
+    },
+    {
+      type: 'list',
+      message: 'Select your license below',
+      choices: ['none',
+        'Apache License 2.0',
+        'GNU General Public License v3.0',
+        'MIT License',
+        'BSD 2-Clause "Simplified" License',
+        'Boost Software License 1.0',
+        'Creative Commons Zero v1.0 Universal',
+        'Eclipse Public License 2.0',
+        'Mozilla Public License 2.0',
+        'The Unlicense'],
+       name: 'license',
+    },
   ])
-]
-  
+
+.then(answers) => {
+    const fileName = `${answers.name.toLowerCase().split(' ').join('')}.md`;
+    const readMeContent = generateMarkdown(answers);
+
+ fs.writeFile(fileName, readMeContent, (err) =>
+      err
+        ? console.log('Something went wrong. Please try again!')
+        : console.log('Input received! Thank you.')
+    ) 
+}
 
 // TODO: Create a function to initialize app
-function init() {
-    return inquirer.prompt(questions)
-        .then((answers)=> {
-                const fileName = `${answers.name.toLowerCase().split(' ').join('')}.md`;
-                const generateReadMe = markDown.generateMarkdown(answers)
-                fs.writeFile(fileName, generateReadMe, (err) =>
-                err ? console.log("Something went wrong. Please try again!") : console.log("Input received! Thank you.")
-                );
-            })
-        }
-    
+// function init() 
+//After you get responses but before you write it to file
+
 // // Function call to initialize app
-init()
+// init()
